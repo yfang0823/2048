d56d8c9b16596024447a462c85884ef1e17f51a9
yfang <yfang0823@163.com>
2017-02-09 16:56:13 +0800

    2048

--- 2048.js
+++ 2048.js
@@ -29,11 +29,25 @@ const loadTable = function() {
     return array
 }
 
+const testArray = [
+    [2, 4, 8, 16],
+    [32, 64, 128, 256],
+    [512, 1024, 2048, 4096],
+    [8192, 16384, 32768, 65536],
+]
+
 // 将数组赋值给html表格
 const saveTable = function(array) {
     var rows = array.length
     var cols = array[0].length
-    
+    var spans = $('table').find('span')
+    for (let i = 0; i < rows; i++) {
+        var arr = array[i]
+        for (let j = 0; j < cols; j++) {
+            var index = i * cols + j
+            spans[index].innerText = arr[j]
+        }
+    }
 }
 
 // 向右相加
